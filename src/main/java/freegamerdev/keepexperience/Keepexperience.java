d4a5661a581aa28b216ed74212b27d1e3bf55190
FweeGamerHD <freegamerhd12@gmail.com>
2024-08-27 15:58:44 +0200

    little bit of code for the mod to be done :P

--- src/main/java/freegamerdev/keepexperience/Keepexperience.java
+++ src/main/java/freegamerdev/keepexperience/Keepexperience.java
@@ -1,10 +1,20 @@
 package freegamerdev.keepexperience;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.minecraft.server.network.ServerPlayerEntity;
 
 public class Keepexperience implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ServerPlayerEvents.AFTER_RESPAWN.register(this::onPlayerRespawn);
+    }
+
+    private void onPlayerRespawn(ServerPlayerEntity oldPlayer, ServerPlayerEntity newPlayer, boolean alive) {
+        if (!alive) {
+            newPlayer.experienceLevel = oldPlayer.experienceLevel;
+            newPlayer.experienceProgress = oldPlayer.experienceProgress;
+        }
     }
 }
