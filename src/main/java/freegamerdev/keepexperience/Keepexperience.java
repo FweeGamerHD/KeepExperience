24e419ffd30f1520ed0d49e78c46a555212dc8e1
FweeGamerHD <freegamerhd12@gmail.com>
2024-08-27 15:24:29 +0200

    Initial commit

---
+++ src/main/java/freegamerdev/keepexperience/Keepexperience.java
@@ -0,0 +1,10 @@
+package freegamerdev.keepexperience;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Keepexperience implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
